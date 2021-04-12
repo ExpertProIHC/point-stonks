988ff55313bbdcb9776a3aa881ccb1f0738c00b0
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-12 13:17:46 -0300

    Revert ":bulb: Add pages app"
    This reverts commit 0066805339f0aa951092f6ac1eb057cbec57dbc8.

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DashboardView, UserView
-
-app_name = 'pages'
-urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
-    path('user', UserView.as_view(), name='user'),
-]
